bf4945db053116c0ff5ad5f07b19a7848dbb84c2
Dmytro Shteflyuk <dmytro@eligible.com>
2018-03-06 16:36:05 -0500

    Simplified validator specs by not creating a new model for each example

--- spec/pwned/pwned_validator_spec.rb
+++ spec/pwned/pwned_validator_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe PwnedValidator do
     attr_accessor :password
   end
 
+  after(:example) do
+    Model.clear_validators!
+  end
+
   describe "when pwned", pwned_range: "5BAA6" do
     it "marks the model as invalid" do
-      class ModelWithValidation < Model
-        validates :password, pwned: true
-      end
-
-      model = ModelWithValidation.new
-      model.password = "password"
+      Model.validates :password, pwned: true
+      model = create_model('password')
 
       expect(model).to_not be_valid
       expect(model.errors[:password].size).to eq(1)
@@ -20,11 +20,8 @@ RSpec.describe PwnedValidator do
     end
 
     it "allows to change the error message" do
-      class ModelWithValidationAndCustomMessage < Model
-        validates :password, pwned: { message: "has been pwned %{count} times" }
-      end
-      model = ModelWithValidationAndCustomMessage.new
-      model.password = "password"
+      Model.validates :password, pwned: { message: "has been pwned %{count} times" }
+      model = create_model('password')
 
       expect(model).to_not be_valid
       expect(model.errors[:password].size).to eq(1)
@@ -32,17 +29,12 @@ RSpec.describe PwnedValidator do
     end
 
     it "allows the user agent to be set" do
-      class ModelWithValidationRequestOptions < Model
-        validates :password, pwned: {
-          request_options: { "User-Agent" => "Super fun user agent" }
-        }
-      end
-
-      model = ModelWithValidationRequestOptions.new
-      model.password = "password"
+      Model.validates :password, pwned: {
+        request_options: { "User-Agent" => "Super fun user agent" }
+      }
+      model = create_model('password')
 
       expect(model).to_not be_valid
-
       expect(a_request(:get, "https://api.pwnedpasswords.com/range/5BAA6").
         with(headers: { "User-Agent" => "Super fun user agent" })).
         to have_been_made.once
@@ -51,12 +43,8 @@ RSpec.describe PwnedValidator do
 
   describe "when not pwned", pwned_range: "37D5B" do
     it "reports the model as valid" do
-      class ModelWithValidation < Model
-        validates :password, pwned: true
-      end
-
-      model = ModelWithValidation.new
-      model.password = "t3hb3stpa55w0rd"
+      Model.validates :password, pwned: true
+      model = create_model('t3hb3stpa55w0rd')
 
       expect(model).to be_valid
     end
@@ -68,34 +56,22 @@ RSpec.describe PwnedValidator do
     end
 
     it "marks the model as valid when not error handling configured" do
-      class ModelWithValidationNoErrorHandling < Model
-        validates :password, pwned: true
-      end
-
-      model = ModelWithValidationNoErrorHandling.new
-      model.password = "password"
+      Model.validates :password, pwned: true
+      model = create_model('password')
 
       expect(model).to be_valid
     end
 
     it "raises a custom error when error handling configured to :raise_error" do
-      class ModelWithValidationRaiseOnError < Model
-        validates :password, pwned: { on_error: :raise_error }
-      end
-
-      model = ModelWithValidationRaiseOnError.new
-      model.password = "password"
+      Model.validates :password, pwned: { on_error: :raise_error }
+      model = create_model('password')
 
       expect { model.valid? }.to raise_error(Pwned::TimeoutError, /execution expired/)
     end
 
     it "marks the model as invalid when error handling configured to :invalid" do
-      class ModelWithValidationInvalidOnError < Model
-        validates :password, pwned: { on_error: :invalid }
-      end
-
-      model = ModelWithValidationInvalidOnError.new
-      model.password = "password"
+      Model.validates :password, pwned: { on_error: :invalid }
+      model = create_model('password')
 
       expect(model).to_not be_valid
       expect(model.errors[:password].size).to eq(1)
@@ -103,12 +79,8 @@ RSpec.describe PwnedValidator do
     end
 
     it "marks the model as invalid with a custom error message when error handling configured to :invalid" do
-      class ModelWithValidationInvalidOnErrorWithCustomMessage < Model
-        validates :password, pwned: { on_error: :invalid, error_message: "might be pwned" }
-      end
-
-      model = ModelWithValidationInvalidOnErrorWithCustomMessage.new
-      model.password = "password"
+      Model.validates :password, pwned: { on_error: :invalid, error_message: "might be pwned" }
+      model = create_model('password')
 
       expect(model).to_not be_valid
       expect(model.errors[:password].size).to eq(1)
@@ -116,25 +88,21 @@ RSpec.describe PwnedValidator do
     end
 
     it "marks the model as valid when error handling configured to :valid" do
-      class ModelWithValidationValidOnError < Model
-        validates :password, pwned: { on_error: :valid }
-      end
-
-      model = ModelWithValidationValidOnError.new
-      model.password = "password"
+      Model.validates :password, pwned: { on_error: :valid }
+      model = create_model('password')
 
       expect(model).to be_valid
     end
 
     it "calls a proc configured for error handling" do
-      class ModelWithValidationProcOnError < Model
-        validates :password, pwned: { on_error: ->(record, error) { raise RuntimeError, "custom proc" } }
-      end
-
-      model = ModelWithValidationProcOnError.new
-      model.password = "password"
+      Model.validates :password, pwned: { on_error: ->(record, error) { raise RuntimeError, "custom proc" } }
+      model = create_model('password')
 
       expect { model.valid? }.to raise_error(RuntimeError, "custom proc")
     end
+  end
+
+  def create_model(password)
+    Model.new.tap { |model| model.password = password }
   end
 end
