7e37371e4a1a7b24e2222c05bd483a24be5ccacb
Phil Nash <philnash@gmail.com>
2018-03-07 00:31:55 +1100

    I prefer begin/rescue/end syntax

--- lib/pwned/password.rb
+++ lib/pwned/password.rb
@@ -35,14 +35,16 @@ module Pwned
     end
 
     def get_hashes
-      open("#{API_URL}#{hashed_password[0..(HASH_PREFIX_LENGTH-1)]}", @request_options) do |io|
-        @hashes = io.read
+      begin
+        open("#{API_URL}#{hashed_password[0..(HASH_PREFIX_LENGTH-1)]}", @request_options) do |io|
+          @hashes = io.read
+        end
+        @hashes
+      rescue Timeout::Error => e
+        raise Pwned::TimeoutError, e.message
+      rescue => e
+        raise Pwned::Error, e.message
       end
-      @hashes
-    rescue Timeout::Error => e
-      raise Pwned::TimeoutError, e.message
-    rescue => e
-      raise Pwned::Error, e.message
     end
 
     def match_data
