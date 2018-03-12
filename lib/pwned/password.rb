0e385f8b92ce0ce7c0cdf07e7a3f2b5a96962d82
Dmytro Shteflyuk <kpumuk@kpumuk.info>
2018-03-12 04:38:55 -0400

    Perf: read the response by line instead of loading the whole thing (#5)
    * Performance: read the response dump line by line instead of loading the whole thing in memory
    
    The response from the service will grow over time. There is no way to get passwords [unpwned](https://github.com/danielmiessler/SecLists/pull/155), so we can safely assume the list will keep growing, adding more an more new hashes. One day it will grow large enough to start taking down servers, when users "DDoS" applications with known "big" pwned password hash prefixes.
    
    This PR switches from "load everything to memory and find our hash" to "fetch data in chunks, and process line by line".
    
    * Remove regular expressions usage in favour of start_with?
    
    In Ruby `start_with?` is heavily optimized compared to regular expressions (more than 2 times faster). This PR replaces regular expressions with `start_with?`
    
    ```
     13.103359   0.734251  13.837610 ( 14.620959)
     13.238428   0.742140  13.980568 ( 14.506166)
     12.836573   0.729563  13.566136 ( 14.191792)
     12.408245   0.642944  13.051189 ( 13.333299)
    ```

--- lib/pwned/password.rb
+++ lib/pwned/password.rb
@@ -7,6 +7,7 @@ module Pwned
   class Password
     API_URL = "https://api.pwnedpasswords.com/range/"
     HASH_PREFIX_LENGTH = 5
+    SHA1_LENGTH = 40
     DEFAULT_REQUEST_OPTIONS = {
       "User-Agent" => "Ruby Pwned::Password #{Pwned::VERSION}"
     }.freeze
@@ -31,27 +32,41 @@ module Pwned
 
     # Returns number of times the password has been pwned.
     def pwned_count
-      regex = /#{Regexp.escape hashed_password[HASH_PREFIX_LENGTH..-1]}:(\d+)/
-      @pwned_count ||= hashes[regex, 1].to_i
+      @pwned_count ||= fetch_pwned_count
     end
 
     private
 
-    def hashes
-      @hashes || get_hashes
+    def fetch_pwned_count
+      suffix = hashed_password_suffix
+      for_each_response_line do |line|
+        next unless line.start_with?(suffix)
+        # Count starts after the suffix, followed by a colon
+        return line[(SHA1_LENGTH-HASH_PREFIX_LENGTH+1)..-1].to_i
+      end
+
+      # The hash was not found, we can assume the password is not pwned [yet]
+      0
     end
 
-    def get_hashes
+    def for_each_response_line(&block)
       begin
-        open("#{API_URL}#{hashed_password[0...HASH_PREFIX_LENGTH]}", @request_options) do |io|
-          @hashes = io.read
+        open("#{API_URL}#{hashed_password_prefix}", @request_options) do |io|
+          io.each_line(&block)
         end
-        @hashes
       rescue Timeout::Error => e
         raise Pwned::TimeoutError, e.message
       rescue => e
         raise Pwned::Error, e.message
       end
+    end
+
+    def hashed_password_prefix
+      hashed_password[0...HASH_PREFIX_LENGTH]
+    end
+
+    def hashed_password_suffix
+      hashed_password[HASH_PREFIX_LENGTH..-1]
     end
   end
 end
